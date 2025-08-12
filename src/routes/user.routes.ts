c4c7cc2d8ec969c7a1d6cbb9005bce235831c37d
James Ovalle <jamesovallebarrera@gmail.com>
2025-08-12 22:33:05 +0200

    Se agrega validacion con zod, se crean los schemas y se ejecuta.

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import * as userController from '../controllers/user.controller';
+import { validate } from '../middlewares/validateResource';
+import { createUserSchema, updateUserSchema } from '../schemas/user.schema';
 
 const router = Router();
 
 router.get('/', userController.getUsers);
-router.post('/', userController.createNewUser); // se envia un json
+router.post('/', validate(createUserSchema), userController.createNewUser); // se envia un json
 router.get('/:id', userController.getUser);
-router.put('/:id', userController.updateExistingUser); // se envia un json
+router.put('/:id', validate(updateUserSchema), userController.updateExistingUser); // se envia un json
 router.delete('/:id', userController.deleteExistingUser); // se envia un id
 
 export default router;
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,31 @@
+import { email, z } from "zod";
+
+// Este objeto zod permite validar la creacion de un usuario
+export const createUserSchema = z.object({
+  body: z.object({
+    name: z.string({error: 'Nombre obligatorio'})
+      .min(3, {message: 'Nombre de mínimo 3 caracteres'}),
+    email: z.email({error: 'Email obligatorio'}),
+    phone: z.string()
+      .regex(/^\+\d{7,15}$/, {message: 'Numero debe empezar con "+" y tener entre 7 a 15 digitos'})
+      .optional()
+  })
+})
+
+export const updateUserSchema = z.object({
+  params: z.object({
+    id: z.string()
+      .refine(val => !isNaN(parseInt(val, 10)), {message: 'Debe ser número'})
+  }),
+  body: z.object({
+    name: z.string()
+      .min(3)
+      .optional(),
+    email: z.email()
+      .optional()
+  })
+    .partial() // partial convierte automaticamente los parametros en optional
+    .refine(data => Object.keys(data).length > 0, {
+      message: 'Mínimo un campo para actualizar'
+    })
+})
